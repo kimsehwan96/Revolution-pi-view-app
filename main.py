4e43cc75150ad5cb62ad2fcf5ac0edaf9b114041
kimsehwanestat <sktpghks138@gmail.com>
2020-07-21 00:16:00 +0900

    add error handler for 404 error

--- main.py
+++ main.py
@@ -29,6 +29,12 @@ def push_values(msg):
     #emit('rtdata', {'data':get_data(profile)})
     emit('rtdata', {'data':random.randint(1,100)})
 
+@app.errorhandler(404) 
+def page_not_found(error): 
+    return render_template('responsive_localview.html')
+#404에러 발생할 경우 (page not found) 메인 페이지로 전환
+
+
 #TODO: request는 1초에 한번씩 들어옴, request요청이 들어왔을 때, 센서 개수만큼의 데이터를 보내줘야함 (array로)
 # 'data' : [데이터1번, 2번, 3번, 4번] -> config.json의 센서 리스트 순서대로
 
